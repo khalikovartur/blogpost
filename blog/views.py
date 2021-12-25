d2b605b2f180bc01fa632d5529acbf28754922c7
artur <khalikart@gmail.com>
2021-12-25 14:40:18 +0500

    added Taggit lib and fitler by tag

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.views.generic import ListView
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from .models import Post, Comment
 from . forms import EmailPostForm, CommentForm
+
+from taggit.models import Tag
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+        
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
+    
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, 
@@ -24,12 +53,7 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html',
                 {'post': post, 'form': form, 'sent':sent})
         
-class PostListView(ListView):
-    queryset = Post.objects.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
-
+        
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,publish__month=month, publish__day=day)
     comments = post.comments.filter(active=True)
@@ -42,6 +66,13 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+        
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags','-publish')[:4]
+    
     
     return render(request, 'blog/post/detail.html',
-                  {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+                  {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #third lib
+    'taggit',
+    
+    #local
     'blog.apps.BlogConfig',
 ]
 
