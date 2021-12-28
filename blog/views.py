19ef4324541d744c11e76a5c658b25570058bd49
artur <khalikart@gmail.com>
2021-12-28 16:48:07 +0500

    extended search on rank, stem and with pg_trgm

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchRank, SearchVector, SearchQuery
+from django.contrib.postgres.search import TrigramSimilarity
 
 from .models import Post, Comment
 from . forms import EmailPostForm, CommentForm, SearchForm
@@ -91,7 +92,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector
-                ('title','body'),).filter(search=query)
+            search_vector = SearchVector('title', weight='A')+ \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(similarity=
+                TrigramSimilarity('title', query),)\
+                .filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html',
         {'form': form, 'query': query, 'results': results})
